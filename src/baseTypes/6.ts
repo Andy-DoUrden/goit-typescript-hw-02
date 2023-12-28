bb65abbf25ead92be3a33ee0a52dca795cb8456f
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-12-28 23:51:04 +0200

    Completed homework

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,17 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+// оскільки змінна називається message то по ідеї тут має бути string але законсольложити ми можемо практично будь що, тому unknown
+function showMessage(message: unknown): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
-  throw new Error('Error');
+function customError(): never {
+  throw new Error("Error");
 }
 
 export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface MyClass {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<MyClass> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
